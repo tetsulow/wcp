ef4b1cfa0f87f6299df9a76b5d68958a49caff24
Miyawaki <tetsu.miyawaki@gmail.com>
2022-06-02 19:03:48 +0900

    Hero

---
+++ JavaWcp/src/overroad09.java
@@ -0,0 +1,8 @@
+public class overroad09 {
+	public static void main(String[] args) {
+		Hero h1 = new Hero("カエデ");
+		System.out.println(h1.name);
+		Hero h2 = new Hero();
+		System.out.println(h2.name);
+	}
+}
